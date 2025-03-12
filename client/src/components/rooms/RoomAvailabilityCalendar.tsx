abd245e953be79658422af8f2e212b6a96a90c59
Harold Aldovino <haroldaldovino@gmail.com>
2025-03-12 23:19:43 +0800

    Change the hero section in homepage

--- client/src/components/rooms/RoomAvailabilityCalendar.tsx
+++ client/src/components/rooms/RoomAvailabilityCalendar.tsx
@@ -4,55 +4,63 @@ const RoomAvailabilityCalendar = () => {
   const currentYear = new Date().getFullYear(); // Get current year dynamically
 
   const defaultArrival = `${currentYear}-03-11`;
-
   const lastDayOfYear = `${currentYear}-12-31`;
 
   const [arrivalDate, setArrivalDate] = useState(defaultArrival);
   const [departureDate, setDepartureDate] = useState(lastDayOfYear);
 
   return (
-    <div className="bg-black/45 pt-6 px-5 mb-3">
-      <div className="text-center w-full max-w-6xl px-5">
-        {/* <div className="border-1 border-white flex-1"></div> */}
-        <h1 className="text-white text-3xl font-light mx-10 whitespace-nowrap">
-          Reservations
-        </h1>
-        {/* <div className="border-1 border-white flex-1"></div> */}
-      </div>
-      <div className="px-8 py-6 pb-10 ">
-        <div className="flex flex-row justify-between items-center gap-10 ">
-          <div className="flex flex-col flex-1">
-            <label
-              htmlFor="arrival"
-              className="text-white/80 text-lg font-thin tracking-tight mb-2"
-            >
-              Arrival Date
-            </label>
-            <input
-              type="date"
-              value={arrivalDate}
-              onChange={(e) => setArrivalDate(e.target.value)}
-              className="border-b-2 border-white outline-0 text-white italic cursor-pointer"
-            />
+    <div className="bg-[#ffffffe6] px-5 mb-6 sm:mb-15 w-full">
+      <div className="py-3 font-montserrat">
+        <div className="flex flex-col justify-around gap-3">
+          {/* Header - Hidden on small screens */}
+          <div className="hidden lg:flex justify-between items-center">
+            <h1 className="text-sm font-montserrat font-medium text-gray-500">
+              <i className="fas fa-calendar-check text-blue-500 text-lg mr-3"></i>
+              Tell us when — your perfect stay awaits you.
+            </h1>
+            <h1 className="text-sm font-montserrat font-medium">
+              Find out more &gt;
+            </h1>
           </div>
-          <div className="flex flex-col flex-1">
-            <label
-              htmlFor="departure"
-              className="text-white/80 text-lg font-thin tracking-tight mb-2 "
-            >
-              Departure Date
-            </label>
-            <input
-              type="date"
-              value={departureDate}
-              onChange={(e) => setDepartureDate(e.target.value)}
-              className="border-b-2 border-white outline-0 text-white italic cursor-pointer"
-            />
-          </div>
-          <div className="flex flex-col">
-            <button className="p-3 font-light text-white bg-transparent border-1 border-white hover:border-violet-700 hover:text-violet-700 transition duration-300 cursor-pointer rounded-sm text-sm">
-              Check Availability
-            </button>
+
+          {/* Form Section */}
+          <div className="flex flex-wrap justify-center items-center gap-3 w-full">
+            {/* Arrival Date */}
+            <div className="flex flex-col min-w-[150px] flex-grow bg-white px-4 py-2">
+              <label htmlFor="arrival" className="text-xs tracking-tight mb-1">
+                Arrival Date
+              </label>
+              <input
+                type="date"
+                value={arrivalDate}
+                onChange={(e) => setArrivalDate(e.target.value)}
+                className="border-b-2 outline-0 italic cursor-pointer"
+              />
+            </div>
+
+            {/* Departure Date */}
+            <div className="flex flex-col min-w-[150px] flex-grow bg-white px-4 py-2">
+              <label
+                htmlFor="departure"
+                className="text-xs tracking-tight mb-1"
+              >
+                Departure Date
+              </label>
+              <input
+                type="date"
+                value={departureDate}
+                onChange={(e) => setDepartureDate(e.target.value)}
+                className="border-b-2 outline-0 italic cursor-pointer"
+              />
+            </div>
+
+            {/* Search Button */}
+            <div className="flex flex-col min-w-[150px] flex-grow">
+              <button className="p-3 py-5 flex-1 bg-blue-600 font-medium transition duration-300 cursor-pointer text-sm text-white w-full">
+                Search
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- client/src/layout/Hero.tsx
+++ client/src/layout/Hero.tsx
@@ -5,39 +5,25 @@ import RoomAvailabilityCalendar from "../components/rooms/RoomAvailabilityCalend
 const Hero = () => {
   return (
     <section
-      className=" h-screen bg-cover bg-center relative before:absolute before:inset-0 before:bg-black/50 before:z-0"
+      className="h-screen bg-cover bg-center relative before:absolute before:inset-0 before:bg-black/60 before:z-0"
       style={{ backgroundImage: `url(${hotel_bg})` }}
     >
       <Navbar />
-      {/* <div className="relative z-10 h-full flex flex-col justify-center px-8 sm:px-15 md:px-20 lg:px-24 xl:px-30 2xl:px-35">
-        <h3 className="text-[clamp(1.5rem,3vw,3rem)] text-white mt-5 mb-4 tracking-wider">
-          Azurea Hotel
-        </h3>
 
-        <h1 className="text-[clamp(2rem,6vw,7rem)] text-white font-bold leading-tight">
-          Stay in <span className="text-[#3C69FF]">Comfort</span>, <br />
-          Leave with <span className="text-[#3C69FF]">Memories</span>
-        </h1>
-
-        <p className="text-[clamp(0.9rem,1.5vw,1.5rem)] text-white mt-4 max-w-4xl leading-relaxed">
-          Beyond a stay, it&apos;s an experience. Relax, unwind, and indulge in
-          the finest <br className="hidden md:block" />
-          comforts designed just for you.
-        </p>
-
-        <button className="mt-8 sm:mt-5 text-[clamp(1rem,1.5vw,1.5rem)] text-white rounded-full border px-6 py-3 font-medium hover:bg-gradient-to-r from-[#7300FF] to-[#08D3FC] transition-all duration-300 group w-fit">
-          Book now
-          <i className="fa-solid fa-arrow-right ml-3 transition-transform duration-300 ease-in-out group-hover:-rotate-45"></i>
-        </button>
-      </div> */}
-      <div className="flex flex-col justify-end items-center h-screen z-10 relative gap-20">
-        <div className="text-white text-center">
-          <h1 className="text-5xl mb-5">The stay you deserve</h1>
-          <p className="text-4xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing.
+      <div className="flex flex-col justify-end items-center h-screen z-10 relative gap-8 sm:gap-14 px-6 sm:px-12 md:px-20">
+        {/* Hero Text */}
+        <div className="text-white text-center max-w-4xl">
+          <h1 className="font-playfair mb-4 text-[clamp(1.75rem,3.5vw,3rem)] leading-tight">
+            Experience Comfort and Luxury
+          </h1>
+          <p className="font-montserrat text-[clamp(0.9rem,1.3vw,1.25rem)] leading-relaxed">
+            Experience the best stay of your life with premium rooms and
+            world-class services today.
           </p>
         </div>
-        <div>
+
+        {/* Calendar */}
+        <div className="w-10/12">
           <RoomAvailabilityCalendar />
         </div>
       </div>
